9269737ad0abfb25dad1aa60e701ab8cbe2f014e
Feysel-6 <cookitcodeitthenliveit@gmail.com>
2026-02-16 01:22:34 +0300

    feat: configure system UI styles status and navigation bars

--- app/mobile/uniconnect/lib/ui/core/theme/theme.dart
+++ app/mobile/uniconnect/lib/ui/core/theme/theme.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:uniconnect/ui/core/theme/colors.dart';
 import 'package:uniconnect/ui/core/theme/custom_themes/elevated_button_theme.dart';
 import 'package:uniconnect/ui/core/theme/custom_themes/outlined_button_theme.dart';
 import 'package:uniconnect/ui/core/theme/custom_themes/text_field_theme.dart';
@@ -7,8 +9,17 @@ import 'package:uniconnect/ui/core/theme/custom_themes/text_theme.dart';
 
 import 'custom_themes/color_scheme_theme.dart';
 
-abstract final class UCTheme{
+abstract final class UCTheme {
   static ThemeData lightTheme = ThemeData(
+    appBarTheme: AppBarTheme(
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+        systemNavigationBarColor: UCColors.background,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    ),
     brightness: Brightness.light,
     textTheme: GoogleFonts.interTextTheme(),
     colorScheme: UCColorScheme.lightScheme,
@@ -17,10 +28,17 @@ abstract final class UCTheme{
   );
 
   static ThemeData darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    textTheme: GoogleFonts.interTextTheme(
-      UCTextTheme.lightTextTheme
+    appBarTheme: AppBarTheme(
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
+        systemNavigationBarColor: UCColors.darkBackground,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
     ),
+    brightness: Brightness.dark,
+    textTheme: GoogleFonts.interTextTheme(UCTextTheme.lightTextTheme),
     inputDecorationTheme: UCTextFormFieldTheme.darkInputDecorationThemeData,
     colorScheme: UCColorScheme.darkScheme,
     // elevatedButtonTheme: UCElevatedButtonTheme.darkElevatedButtonThemeData,
