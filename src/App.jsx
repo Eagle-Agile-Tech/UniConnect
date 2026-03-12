70009b8517c7aa5c601bee247707d7ac7586af94
imanyilma <155858994+imanyilma@users.noreply.github.com>
2026-03-12 15:01:12 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React ,{useState}from 'react'
+import Header from './components/layout/header'
+import Sidebar from './components/layout/sidebar'
+import Dashboard from './components/Dashbord/dashboard'
+export default function App() {
+  const [sideBarCollapsed, setSideBarCollapsed] = useState(false);
+  const [CurrentPage, setCurrentPage] = useState("dashboard");
+  return (
+    <div className="min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50 
+    dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500">
+      <div className="flex h-screen overflow-hidden">
+
+        <Sidebar
+         collapsed={sideBarCollapsed} 
+        onToggle={() => setSideBarCollapsed(!sideBarCollapsed)}
+        CurrentPage={CurrentPage}
+        onPageChange={setCurrentPage}
+        />
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <Header 
+          sideBarCollapsed={sideBarCollapsed}
+          onToggleSidebar={() => setSideBarCollapsed(!sideBarCollapsed)}
+          />
+          <main className="flex-1 overflow-y-auto bg-transparent">
+            <div className="p-6 space-y-6">
+             {CurrentPage === "dashboard" && <Dashboard/>}
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Dashbord/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function dashboard() {
+  return (
+
+    <div classname="space-y-6">
+      {/* status grid */}
+      d
+
+    </div>
+  )
+}
